e64543ba3096b25c38f2f8072cc59a6fcf9e3319
Raphael Colman <raphael.colman@infinityworks.com>
2019-12-01 19:39:35 +0000

    Add tests

---
+++ src/RocketEquationTest.hs
@@ -0,0 +1,19 @@
+module RocketEquationSpec where
+
+import Test.Hspec
+import RocketEquation
+
+main :: IO ()
+main = hspec $ do
+  describe "FuelRequiredSimple" $ do
+    it "FuelRequiredSimple should calculate correctly" $ do
+      fuelRequiredSimple 12 `shouldBe` 2
+      fuelRequiredSimple 14 `shouldBe` 2
+      fuelRequiredSimple 1969 `shouldBe` 654
+      fuelRequiredSimple 100756 `shouldBe` 33583
+
+    it "FuelRequired should include fuel calculation" $ do
+      fuelRequired 12 `shouldBe` 2
+      fuelRequired 14 `shouldBe` 2
+      fuelRequired 1969 `shouldBe` 966
+      fuelRequired 100756 `shouldBe` 50346
