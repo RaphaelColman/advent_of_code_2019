92a4b89a286935b3bc9083058967924a4d3e4ff1
Raphael Colman <raphael.colman@infinityworks.com>
2019-12-03 18:45:27 +0000

    Add helper functions

--- src/AoC2/ProgramAlarm.hs
+++ src/AoC2/ProgramAlarm.hs
@@ -5,5 +5,16 @@ intcode xs = [1,2,3,4]
 
 applyIntcode :: (Num a) => [a] -> Integer -> [a]
 applyIntcode = undefined
+applyIntcode xs index = sliceList index (index + 4) xs
 
-data Operation = Operation {operation :: Integer, first :: Integer, second :: Integer, location :: Integer} deriving (Show)
+applyOperation :: Operation -> [Int] -> [Int]
+applyOperation (Operation o f s l) xs
+  | o == 1 = replaceInList l (f+s)
+  | o == 2 = replaceInList l (f*s)
+  | o == 99 = xs
+  where replaceInList index value = take index xs ++ [value] ++ drop (index + 1) xs
+
+sliceList :: Int -> Int -> [a] -> [a]
+sliceList from to xs = take (to - from + 1) (drop from xs)
+
+data Operation = Operation {operation :: Int, first :: Int, second :: Int, location :: Int} deriving (Show)
