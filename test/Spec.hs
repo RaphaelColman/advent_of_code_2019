923a7b622884050b18ba294eb3312b845347f784
Raphael.Colman <rcolman@ybs.com>
2019-12-03 09:04:58 +0000

    Manual test list until I can get test discovery to work

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,13 @@
 import Test.Hspec
 
-import qualified RocketEquationSpec
+import qualified AoC1.RocketEquationSpec
+import qualified AoC2.ProgramAlarmSpec
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "RocketEquation"     RocketEquationSpec.spec
+  describe "RocketEquation"     AoC1.RocketEquationSpec.spec
+  describe "ProgramAlarm"       AoC2.ProgramAlarmSpec.spec
 
