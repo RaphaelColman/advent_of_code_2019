07a831d4ecfa254b3c721755cc9b9ae7af52e895
Raphael.Colman <rcolman@ybs.com>
2019-12-03 16:42:30 +0000

    Fix AoC2 tests

--- src/AoC2/ProgramAlarm.hs
+++ src/AoC2/ProgramAlarm.hs
@@ -2,3 +2,8 @@ module AoC2.ProgramAlarm where
 
 intcode :: (Num a) => [a] -> [a]
 intcode xs = [1,2,3,4]
+
+applyIntcode :: (Num a) => [a] -> Integer -> [a]
+applyIntcode = undefined
+
+data Operation = Operation {operation :: Integer, first :: Integer, second :: Integer, location :: Integer} deriving (Show)
