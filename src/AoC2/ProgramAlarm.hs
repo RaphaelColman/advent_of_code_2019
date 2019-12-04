8afd55e218dfe543d774a999431af15d13e11197
Raphael <raphael.colman@infinityworks.com>
2019-12-04 09:00:39 +0000

    solved part 1

--- src/AoC2/ProgramAlarm.hs
+++ src/AoC2/ProgramAlarm.hs
@@ -1,17 +1,35 @@
 module AoC2.ProgramAlarm where
 
+import Data.List.Split
+
+main :: IO ()
+main = do
+  contents <- getContents
+  putStrLn $ show $ intcode (splitToIntList "," contents)
+
+splitToIntList :: String -> String -> [Int]
+splitToIntList delim strList = map read $ splitOn delim strList
+
 intcode :: [Int] -> [Int]
-intcode xs = go xs xs 
-  where go [] newList = newList
-        go (a:b:c:d:xs) newList' = go (drop 4 mutateList) mutateList
-          where mutateList = (applyOperation a b c d newList')
-        go (y:ys) newList'' = newList''
+intcode xs = go xs 0 
+  where go [] _ = []
+        go xs i
+            | i >= (length xs) = xs
+            | xs !! i == 99 = xs
+            | otherwise = go (intcodeAtIndex xs i) (i+4)
 
 applyOperation :: Int -> Int -> Int -> Int -> [Int] -> [Int]
 applyOperation o f s l xs
   | o == 1 = replaceInList l ((xs !! f) + (xs !! s)) xs
   | o == 2 = replaceInList l ((xs !! f) * (xs !! s)) xs
-  | o == 99 = xs
+  | o == 99 = error ("Should not have got opcode 99 in here: " ++ show o)
+  | otherwise = error ("Unknown opcode: " ++ show o)
+
+intcodeAtIndex :: [Int] -> Int -> [Int]
+intcodeAtIndex xs i = let slice = sliceList i (i+4) xs
+                      in go slice xs
+                     where go (a:b:c:d:as) xs = applyOperation a b c d xs
+                           go h i = error (show h ++ show i)
 
 replaceInList :: Int -> a -> [a] -> [a]
 replaceInList index value xs = take index xs ++ [value] ++ drop (index + 1) xs
--- test/AoC2/ProgramAlarmSpec.hs
+++ test/AoC2/ProgramAlarmSpec.hs
@@ -11,3 +11,6 @@ spec = do
       intcode ([2,3,0,3,99] :: [Int]) `shouldBe` ([2,3,0,6,99] :: [Int])
       intcode ([2,4,4,5,99,0] :: [Int]) `shouldBe` ([2,4,4,5,99,9801] :: [Int])
       intcode ([1,1,1,4,99,5,6,0,99] :: [Int]) `shouldBe` ([30,1,1,4,2,5,6,0,99] :: [Int])
+
+    it "intcodeAtIndex should apply intcode" $ do
+      intcodeAtIndex [1,1,1,4,2,5,6,0,99] 4 `shouldBe` [30,1,1,4,2,5,6,0,99]
