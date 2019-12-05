6cbf0b776a10793be5e847fe292af603800b8820
Raphael Colman <raphael.colman@infinityworks.com>
2019-12-05 19:09:20 +0000

    Directions and parsing

--- src/AoC3/CrossedWires.hs
+++ src/AoC3/CrossedWires.hs
@@ -1,5 +1,7 @@
 module AoC3.CrossedWires where
 
+import qualified Data.Map as Map
+
 closestIntersection :: String -> Integer
 closestIntersection = undefined
 
@@ -9,6 +11,18 @@ allPointsFromLine = undefined
 up :: (Num a, Enum a, Num b, Enum b) => b -> (a, b) -> [(a, b)]
 up amount (startx,starty) = [(x,y) | x <- [startx], y <- [starty+1..starty + amount]]
 
+down :: (Num a, Enum a, Num b, Enum b) => b -> (a, b) -> [(a, b)]
+down amount (startx, starty) = [(x,y) | x <- [startx], y <- [starty-1, starty-2..starty-amount]]
+
 right :: (Num a, Enum a, Num b, Enum b) => a -> (a, b) -> [(a, b)]
 right amount (startx,starty) = [(x,y) | x <- [startx+1..startx + amount], y <- [starty]]
 
+left :: (Num a, Enum a, Num b, Enum b) => a -> (a, b) -> [(a, b)]
+left amount (startx, starty) = [(x,y) | x <- [startx-1,(startx-2)..startx-amount], y <- [starty]]
+
+parseDirection :: (Num a, Enum a) => [Char] -> (a -> (a, a) -> [(a, a)])
+parseDirection dir = case dir of
+                       "R" -> right
+                       "L" -> left
+                       "U" -> up
+                       "D" -> down
