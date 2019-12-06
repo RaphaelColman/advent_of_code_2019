60abce061cfa653c666a5945a5c87875fc4e7203
Raphael Colman <raphael.colman@infinityworks.com>
2019-12-06 23:08:12 +0000

    A terrible attempt at Day 3

--- src/AoC3/CrossedWires.hs
+++ src/AoC3/CrossedWires.hs
@@ -1,12 +1,39 @@
 module AoC3.CrossedWires where
 
-import qualified Data.Map as Map
+import qualified Data.Map as M
+import qualified Data.Set as S
+import Data.List.Split
+
+parseInput :: String -> [[String]]
+parseInput = (map (\x -> splitOn "," x)) . (splitOn "\n")
 
 closestIntersection :: String -> Integer
-closestIntersection = undefined
+closestIntersection = closestIntersectionImpl . parseInput
 
-allPointsFromLine :: [String] -> [(Integer, Integer)]
-allPointsFromLine = undefined
+closestIntersectionImpl :: [[String]] -> Integer
+closestIntersectionImpl = minimum
+                        . (map $ manhattan (0,0))
+                        . (filter (/= (0,0)))
+                        . allCrossedPoints
+
+allCrossedPoints :: [[String]] -> [(Integer, Integer)]
+allCrossedPoints xs = map (\x -> fst x) $ M.toList $ M.filter (>1) $  freqs $ allPointsFromLines xs
+
+allPointsFromLines :: [[String]] -> [(Integer, Integer)]
+allPointsFromLines xs = concat $ map line xs
+
+-- I need a cartesian function. Applicatives don't work because they combine with themselves
+
+crossings :: Line -> Line -> [Point]
+crossings l1 l2 = (filter (\x -> x /= (0,0)))
+                $ (S.toList $ S.intersection (S.fromList l1) (S.fromList l2))
+
+type Point = (Integer, Integer)
+type Line = [Point]
+
+line :: [String] -> Line 
+line = foldl doLine [(0,0)]
+  where doLine = (\x y -> x ++ ((parseDirection (head y)) (read (tail y) :: Integer) (last x)))
 
 up :: (Num a, Enum a, Num b, Enum b) => b -> (a, b) -> [(a, b)]
 up amount (startx,starty) = [(x,y) | x <- [startx], y <- [starty+1..starty + amount]]
@@ -20,9 +47,15 @@ right amount (startx,starty) = [(x,y) | x <- [startx+1..startx + amount], y <- [
 left :: (Num a, Enum a, Num b, Enum b) => a -> (a, b) -> [(a, b)]
 left amount (startx, starty) = [(x,y) | x <- [startx-1,(startx-2)..startx-amount], y <- [starty]]
 
-parseDirection :: (Num a, Enum a) => [Char] -> (a -> (a, a) -> [(a, a)])
+parseDirection :: (Num a, Enum a) => Char -> (a -> (a, a) -> [(a, a)])
 parseDirection dir = case dir of
-                       "R" -> right
-                       "L" -> left
-                       "U" -> up
-                       "D" -> down
+                       'R' -> right
+                       'L' -> left
+                       'U' -> up
+                       'D' -> down
+
+manhattan :: (Integer, Integer) -> (Integer, Integer) -> Integer
+manhattan (a, b) (c, d) = abs $ (c-a) + (d-b)
+
+freqs :: (Ord k, Num a) => [k] -> M.Map k a
+freqs xs = M.fromListWith (+) $ (map (\x -> (x, 1)) xs)
--- test/AoC3/CrossedWiresSpec.hs
+++ test/AoC3/CrossedWiresSpec.hs
@@ -6,13 +6,14 @@ import AoC3.CrossedWires
 spec :: Spec
 spec = do
   describe "Completion test" $ do
-    xit "Inputs should get correct output" $ do
-      --Should newlines become spaces or some other delimiter?
-      closestIntersection "R75,D30,R83,U83,L12,D49,R71,U7,L72 U62,R66,U55,R34,D71,R55,D58,R83" `shouldBe` 159
-      closestIntersection "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51 U99,R91,D20,R16,D67,R40,U7,R15,U6,R7" `shouldBe` 135
+    it "Inputs should get correct output" $ do
+      closestIntersection "D3,R3,U6\nR6" `shouldBe` 3
+      --closestIntersection "R75,D30,R83,U83,L12,D49,R71,U7,L72" `shouldBe` 159 -- This is clearly wrong, but I'm leaving it in because it proves that this line crosses itself
+      --closestIntersection "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU99,R91,D20,R16,D67,R40,U7,R15,U6,R7" `shouldBe` 135
+      --closestIntersection "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83" `shouldBe` 159
 
-    --it "should calculate all points covered by a line" $ do
-    --  allPointsFromLine Line ["R2", "D4", "L3"] `shouldBe` [(0,0), (0,1), (-1, 1), (-2, 1), (-3,1), (-4,1), (-4,0), (-3,0), (-2,0)]
+    it "should calculate all points covered by a line" $ do
+      line ["R2", "D4", "L3"] `shouldBe` [(0,0), (1,0), (2,0), (2,-1), (2,-2), (2,-3), (2,-4), (1,-4),(0,-4),(-1,-4)]
 
     it "produces correct tuples from direction" $ do
       up 2 (0,0) `shouldBe` [(0,1), (0,2)]
@@ -20,4 +21,13 @@ spec = do
       left 3 (2,2) `shouldBe` [(1,2), (0,2), (-1,2)]
       down 1 (0,0) `shouldBe` [(0,-1)]
 
+    it "should calculate manhattan distance" $ do
+      manhattan (3,4) (0,0) `shouldBe` 7
+      manhattan (-4,-9) (0,0) `shouldBe` 13
+
+    it "should find all crossings between two lines" $ do
+      (crossings (line ["D3","R3","U6"]) (line ["R6"])) `shouldBe` [(3,0)]
+
+    it "should find all crossigns between a list of lines" $ do
+      (allCrossings [(line ["D3","R3","U6"]),(line ["R6"])]) `shouldBe` [[(3,0)]]
 
