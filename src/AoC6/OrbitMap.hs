e41b80751e4563ac7d79a4b06b6633494a6d8909
Raphael Colman <raphael.colman@infinityworks.com>
2019-12-14 20:55:38 +0000

    Day 6 part 2

--- src/AoC6/OrbitMap.hs
+++ src/AoC6/OrbitMap.hs
@@ -3,12 +3,14 @@ module AoC6.OrbitMap where
 import qualified Data.Map.Lazy as Map
 import Data.List.Split
 import System.IO
+import qualified Data.Set as Set
 
 main :: IO ()
 main = do
   handle <- openFile "src/AoC6/input.txt" ReadMode  
   contents <- hGetContents handle
   print $ calculateOrbits $ parseOrbits contents
+  print $ calculateShortestPath "YOU" "SAN" $ parseOrbits contents
 
 calculateOrbits :: Map.Map String String -> Int
 calculateOrbits orbitMap = sum $ Map.map (calcOrbits . Just) orbitMap
@@ -23,3 +25,16 @@ parseOrbit orbitStr = let [child, parent] = splitOn ")" orbitStr in
 --Function composition FTW!!!
 parseOrbits :: String -> Map.Map String String
 parseOrbits = Map.fromList . map parseOrbit . lines
+
+calculateShortestPath :: String -> String -> Map.Map String String -> Int
+calculateShortestPath you santa orbitMap = let youChildren = Set.fromList (getAllChildren you orbitMap);
+                                                santaChildren = Set.fromList (getAllChildren santa orbitMap);
+                                                justYourChildren = Set.size $ Set.difference youChildren santaChildren;
+                                                justSantasChildren = Set.size $ Set.difference santaChildren youChildren in
+                                                  justYourChildren + justSantasChildren
+
+
+getAllChildren :: String -> Map.Map String String -> [String]
+getAllChildren start orbitMap = case Map.lookup start orbitMap of
+  Just child -> child : getAllChildren child orbitMap
+  Nothing -> []
--- test/AoC6/OrbitMapSpec.hs
+++ test/AoC6/OrbitMapSpec.hs
@@ -3,6 +3,7 @@ module AoC6.OrbitMapSpec (spec) where
 import Test.Hspec
 import AoC6.OrbitMap
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 
 
 spec :: Spec
@@ -15,7 +16,13 @@ spec =  do
   describe "Parsing an orbit string" $ 
     it "can turn an orbit string into a tuple" $
       parseOrbit "child)parent" `shouldBe` ("parent", "child")
+    
+  describe "Calculating the shortest distance" $
+      it "Can calculate the shortest distance between two points" $
+        calculateShortestPath "YOU" "SAN" complexMapWithYouAndSanta `shouldBe` 4
 
+
+  
 
 
 simpleMap :: Map.Map String String 
@@ -23,3 +30,6 @@ simpleMap = Map.fromList [("B", "Com"), ("C", "B")]
 
 complexMap :: Map.Map String String
 complexMap = Map.fromList [("B","COM"), ("C","B"), ("D","C"), ("E","D"), ("F","E"), ("G","B"), ("H","G"), ("I","D"), ("J","E"), ("K","J"), ("L","K")]
+
+complexMapWithYouAndSanta :: Map.Map String String
+complexMapWithYouAndSanta = Map.fromList [("B","COM"), ("C","B"), ("D","C"), ("E","D"), ("F","E"), ("G","B"), ("H","G"), ("I","D"), ("J","E"), ("K","J"), ("L","K"), ("YOU", "K"), ("SAN", "I")]
