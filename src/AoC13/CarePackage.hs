73a0831e90f6a0ed46bd3eb76332b7783deee8b8
Raphael Colman <raphael.colman@infinityworks.com>
2019-12-22 22:43:23 +0000

    Can print maps

--- src/AoC13/CarePackage.hs
+++ src/AoC13/CarePackage.hs
@@ -6,10 +6,12 @@ import System.IO
 import Linear.V2
 import Control.Lens hiding (Empty)
 import Control.Lens.TH
-import qualified Data.Map as Map
 import Data.List.Split
 import Debug.Trace
-
+import qualified Data.Map as Map
+import Data.Map(Map(..))
+import Data.List
+import Data.Maybe
 
 type Point = V2 Int
 data TileType = Empty | Wall | Block | Paddle | Ball deriving (Show, Eq, Enum, Bounded)
@@ -25,7 +27,9 @@ main = do
     fileHandle <- openFile "src/AoC13/input.txt" ReadMode
     contents <- hGetContents fileHandle
     let outputs' =  reverse . outputs <$> runIntCode (parse contents)
+    let tiles = outputs' >>= parseOutput
     print $ numBlockTiles <$> (outputs' >>= parseOutput)
+    putStr $ fromJust $ renderVectorMap <$> tilesToMap <$> tiles
 
 tileForInt :: Int -> Maybe TileType
 tileForInt = flip Map.lookup tileMap
@@ -42,3 +46,25 @@ parseOutput xs = mapM makeTile chunks
 numBlockTiles :: [Tile] -> Int
 numBlockTiles = length . filter isBlock
     where isBlock t = t ^. tileType == Block
+
+tilesToMap :: [Tile] -> Map (V2 Int) Char
+tilesToMap = Map.fromList . map tileToTup
+    where tileToTup (Tile type' position') = (position', typeToChar type')
+          typeToChar t = case t of
+            Empty -> '.'
+            Wall -> '+'
+            Block -> '#'
+            Paddle -> '_'
+            Ball -> 'o'
+
+renderVectorMap :: Map (V2 Int) Char -> String
+renderVectorMap m = foo
+    where keys = Map.keys m
+          xMax = maximumBy (\a b -> compare (a ^._x) (b ^._x)) keys ^._x
+          xMin = minimumBy (\a b -> compare (a ^._x) (b ^._x)) keys ^._x
+          yMax = maximumBy (\a b -> compare (a ^._y) (b ^._y)) keys ^._y
+          yMin = minimumBy (\a b -> compare (a ^._y) (b ^._y)) keys ^._y
+          xRange = (xMax - xMin) + 1
+          panelList = [Map.findWithDefault '.' (V2 x y) m | y <- [yMin .. yMax], x <- [xMin..xMax]]
+          panelRows = chunksOf xRange panelList
+          foo = unlines panelRows
